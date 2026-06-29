e44397fb67080b4ba9fd1296b40784359bad2966
RohatErgun <rohatergun06@gmail.com>
2026-06-29 12:52:52 +0200

    initial setup

---
+++ src/main/java/io/github/rohergun/recipe_hub/RecipeHubApplication.java
@@ -0,0 +1,13 @@
+package io.github.rohergun.recipe_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rohergun/recipe_hub/RecipeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rohergun.recipe_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
