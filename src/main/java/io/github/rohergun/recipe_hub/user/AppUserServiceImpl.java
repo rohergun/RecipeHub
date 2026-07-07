ba22c4bb0deb09ff00d4897763fd30db1abb3fbf
RohatErgun <rohatergun06@gmail.com>
2026-07-07 12:43:10 +0200

    fix: improve error handling in AppUserServiceImpl

--- src/main/java/io/github/rohergun/recipe_hub/user/AppUserServiceImpl.java
+++ src/main/java/io/github/rohergun/recipe_hub/user/AppUserServiceImpl.java
@@ -1,5 +1,7 @@
 package io.github.rohergun.recipe_hub.user;
 
+import io.github.rohergun.recipe_hub.exception.DomainErrorMessage;
+import io.github.rohergun.recipe_hub.exception.RecipeHubException;
 import io.github.rohergun.recipe_hub.user.dtos.UserProfileResponse;
 import io.github.rohergun.recipe_hub.user.dtos.UserProfileUpdateRequest;
 import lombok.RequiredArgsConstructor;
@@ -17,18 +19,18 @@ public class AppUserServiceImpl implements AppUserService{
     @Override
     public UserProfileResponse getUserProfile(String username) {
         AppUser user = userRepo.findByUsername(username)
-                .orElseThrow(() -> new NoSuchElementException("User not found"));
+                .orElseThrow(() -> new RecipeHubException(DomainErrorMessage.USER_NOT_FOUND));
         return userMapper.toResponse(user);
     }
 
     @Override
     public UserProfileResponse updateUserProfile(UUID id, UserProfileUpdateRequest request) {
         AppUser user = userRepo.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("User not found"));
+                .orElseThrow(() -> new RecipeHubException(DomainErrorMessage.USER_NOT_FOUND));
 
         if (!user.getUsername().equals(request.username()) &&
             userRepo.existsByUsername(request.username())) {
-            throw new IllegalArgumentException("Username is already taken");
+            throw new RecipeHubException(DomainErrorMessage.USERNAME_ALREADY_EXISTS);
         }
 
         userMapper.updateUserFromRequest(request, user);
@@ -40,7 +42,7 @@ public class AppUserServiceImpl implements AppUserService{
     @Override
     public void deleteUser(UUID id) {
         AppUser user = userRepo.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("User not found"));
+                .orElseThrow(() -> new RecipeHubException(DomainErrorMessage.USER_NOT_FOUND));
         userRepo.delete(user);
     }
 }
