ee450c5c4fb72d9c6a2914f21166a6746491c9b8
RohatErgun <rohatergun06@gmail.com>
2026-07-10 14:49:43 +0200

    feat: add CustomDetailsService and add findByEmail method to AppUserRepository

---
+++ src/main/java/io/github/rohergun/recipe_hub/security/CustomDetailsService.java
@@ -0,0 +1,24 @@
+package io.github.rohergun.recipe_hub.security;
+
+import io.github.rohergun.recipe_hub.exception.DomainErrorMessage;
+import io.github.rohergun.recipe_hub.exception.RecipeHubException;
+import io.github.rohergun.recipe_hub.user.AppUser;
+import io.github.rohergun.recipe_hub.user.AppUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomDetailsService implements UserDetailsService {
+    private final AppUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        AppUser user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RecipeHubException(DomainErrorMessage.USER_NOT_FOUND));
+
+        return new CustomUserDetails(user);
+    }
+}
--- src/main/java/io/github/rohergun/recipe_hub/user/AppUserRepository.java
+++ src/main/java/io/github/rohergun/recipe_hub/user/AppUserRepository.java
@@ -10,4 +10,5 @@ import java.util.UUID;
 public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
     Optional<AppUser> findByUsername(String username);
     boolean existsByUsername(String username);
+    Optional<AppUser> findByEmail(String email);
 }
