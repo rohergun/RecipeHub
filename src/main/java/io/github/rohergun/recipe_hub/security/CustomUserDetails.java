5293030d410aba8484945aeec5060bf526db386d
RohatErgun <rohatergun06@gmail.com>
2026-07-10 14:43:36 +0200

    feat: add CustomUserDetails implements UserDetails

---
+++ src/main/java/io/github/rohergun/recipe_hub/security/CustomUserDetails.java
@@ -0,0 +1,62 @@
+package io.github.rohergun.recipe_hub.security;
+
+import io.github.rohergun.recipe_hub.user.AppUser;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final AppUser user;
+
+    public UUID getId() {
+        return user.getId();
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    /**
+     * returning to email of user instead of username.
+     * <p></p>
+     * email is unique for each account and used in login.
+     * */
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
