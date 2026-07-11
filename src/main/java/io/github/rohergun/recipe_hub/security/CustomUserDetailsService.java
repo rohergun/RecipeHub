5184d19e56355802fd2082adeb918e1b329acac6
RohatErgun <rohatergun06@gmail.com>
2026-07-11 12:13:47 +0200

    refactor: update name to CustomUserDetailsService

--- src/main/java/io/github/rohergun/recipe_hub/security/CustomUserDetailsService.java
+++ src/main/java/io/github/rohergun/recipe_hub/security/CustomUserDetailsService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     private final AppUserRepository userRepository;
 
     @Override
