03a3b4cb4f82a87812620fdeeaefd2a176784742
RohatErgun <rohatergun06@gmail.com>
2026-07-21 09:58:08 +0200

    fix: add NotBlank annotation to username in RegisterRequest

--- src/main/java/io/github/rohergun/recipe_hub/auth/dtos/RegisterRequest.java
+++ src/main/java/io/github/rohergun/recipe_hub/auth/dtos/RegisterRequest.java
@@ -10,8 +10,10 @@ public record RegisterRequest (
         @Email @NotBlank
         String email,
 
+        @NotBlank
         @Size(min = 2, max = 20, message = "Username length must be between {min} and {max} characters.")
         String username,
+
         @NotBlank @Size(min = 6, max = 50)
         @Pattern(
                 regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d]).{6,50}$",
